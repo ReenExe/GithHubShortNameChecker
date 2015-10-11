dc12318b309eacdb507454bec9b00993b23eb0a2
ReenExe <ipourri@gmail.com>
2015-10-11 06:10:35 +0300

    use component Process

--- src/AppBundle/Command/CheckFreeNameProcessCommand.php
+++ src/AppBundle/Command/CheckFreeNameProcessCommand.php
@@ -5,19 +5,19 @@ namespace AppBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Process;
 
-class CheckFreeNameCommand extends ContainerAwareCommand
+class CheckFreeNameProcessCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('fn:check:name');
+        $this->setName('fn:check:name:process');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $process = new Process('app/console fn:check:name:process');
-        $process->run();
-        $output->writeln($process->getOutput());
+        $startTime = microtime(true);
+        $this->getContainer()->get('fn.check_name_service')->progress(1);
+        $duration = microtime(true) - $startTime;
+        $output->writeln("Duration: <info>$duration</info>");
     }
 }

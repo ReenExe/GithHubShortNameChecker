a40471c2af038daf14d355b3128457d2fb8c7196
ReenExe <ipourri@gmail.com>
2015-10-16 19:55:40 +0300

    mysql access

--- vagrant/mysql.sh
+++ vagrant/mysql.sh
@@ -3,3 +3,4 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password empty"
 sudo aptitude install -q -y -f  mysql-server mysql-client php5-mysql
 mysql -uroot -pempty -e 'CREATE DATABASE `githubfreenames` CHARACTER SET `utf8` COLLATE `utf8_bin`;'
+mysql -uroot -pempty -e "CREATE USER 'super'@'%' IDENTIFIED BY 'empty'; grant all on *.* to 'super'@'%' with grant option;"
